00f19b7d52c53c362f9919985f9a6911a9b01cb9
Markus Klein <m@mklein.co.at>
2016-05-19 22:43:51 +0200

    add chatroom iml and java server implementation

---
+++ server/src/ChatroomImpl.java
@@ -0,0 +1,39 @@
+import chat.ChatroomPOA;
+import chat.InvalidConnectionIdException;
+import chat.Listener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by markus on 19/05/16.
+ */
+public class ChatroomImpl extends ChatroomPOA {
+    private Map<Integer, Listener> listeners = new HashMap<>();
+    private int messageIdCounter = 0;
+
+    @Override
+    public int register(Listener client, String listenerName) {
+        int messageId = messageIdCounter;
+        messageIdCounter++;
+
+        listeners.put(messageId, client);
+        return messageId;
+    }
+
+    @Override
+    public void send(int connectionId, String message) throws InvalidConnectionIdException {
+        System.out.println(message);
+        for (Map.Entry<Integer, Listener> pair : listeners.entrySet()) {
+            //if (!pair.getKey().equals(connectionId)) {
+            System.out.println("sending to: " + pair.getKey());
+                pair.getValue().receive(message);
+            //}
+        }
+    }
+
+    @Override
+    public void unregister(int connectionId) throws InvalidConnectionIdException {
+        listeners.remove(connectionId);
+    }
+}
---
+++ server/src/Server.java
@@ -0,0 +1,48 @@
+import chat.Chatroom;
+import chat.ChatroomHelper;
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContext;
+import org.omg.CosNaming.NamingContextHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
+/**
+ * Created by markus on 19/05/16.
+ */
+public class Server {
+    public static void main(String[] args)  {
+        try {
+            ORB orb = ORB.init(args, null);
+            System.out.println("Initialized ORB");
+
+            //Instantiate Servant and create reference
+            POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            ChatroomImpl chImpl = new ChatroomImpl();
+            rootPOA.activate_object(chImpl);
+            Chatroom chRef = ChatroomHelper.narrow(rootPOA.servant_to_reference(chImpl));
+
+            //Bind reference with NameService
+            NamingContext namingContext = NamingContextHelper.narrow(orb.resolve_initial_references("NameService"));
+            System.out.println("Resolved NameService");
+            NameComponent[] nc = { new NameComponent("Chatroom", "") };
+            namingContext.rebind(nc, chRef);
+
+            //Activate rootpoa
+            rootPOA.the_POAManager().activate();
+
+            //Start readthread and wait for incoming requests
+            System.out.println("Server ready and running ....");
+
+            orb.run();
+
+        }	catch (Exception e)	{
+            System.err.println("Es ist ein Fehler aufgetreten: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public Server() {
+
+    }
+}
