7e2be5033b15ade2cb9858d56559515abd3c6c3c
Markus Klein <m@mklein.co.at>
2016-05-26 16:31:23 +0200

    restrucutre python client

--- client_python/client.py
+++ client_python/client.py
@@ -11,67 +11,81 @@ from omniORB import CORBA
 import CosNaming
 import chat__POA, chat
 
-# listener implementation
-class Listener_I(chat__POA.Listener):
-    def receive(self, msg):
-        print(msg)
+
+class ChatroomClient:
+    def __init__(self, orb_args):
+        # Initialise the ORB
+        self.orb = CORBA.ORB_init(orb_args)
+
+        # Obtain a reference to the root naming context
+        obj = self.orb.resolve_initial_references("NameService")
+        rootContext = obj._narrow(CosNaming.NamingContext)
+
+        self.poa = self.orb.resolve_initial_references("RootPOA")
+
+        if rootContext is None:
+            print "Failed to narrow the root naming context"
+            sys.exit(1)
+
+        # Resolve the name "test.my_context/ExampleEcho.Object"
+        name = [CosNaming.NameComponent("Chatroom", "")]
+
+        try:
+            obj = rootContext.resolve(name)
+
+        except CosNaming.NamingContext.NotFound, ex:
+            print "Name not found"
+            sys.exit(1)
+
+        # Narrow the object to an Example::Echo
+        self.chatroom = obj._narrow(chat.Chatroom)
+        if self.chatroom is None:
+            print "Object reference is not an Example::Echo"
+            sys.exit(1)
+
+    def register(self, listener, listener_name):
+        # register the listener
+        listener_ref = listener._this()
+        return self.chatroom.register(listener_ref, listener_name)
+
+    def send(self, listener_id, msg):
+        self.chatroom.send(listener_id, msg)
+
+    def run(self):
+        poa_manager = self.poa._get_the_POAManager()
+        poa_manager.activate()
+        self.orb.run()
+
+    def shutdown(self):
+        self.chatroom.unregister(id)
+        self.orb.shutdown(True)
 
 
-# sender thread
-def send_thread(chatroom, id):
-    while(1):
-        msg = raw_input("> ")
-        chatroom.send(id, msg)
+if __name__ == '__main__':
+    # listener implementation
+    class ChatroomListener(chat__POA.Listener):
+        def receive(self, msg):
+            print(msg)
 
-# Initialise the ORB
-orb = CORBA.ORB_init(sys.argv[2:])
 
-# Obtain a reference to the root naming context
-obj = orb.resolve_initial_references("NameService")
-rootContext = obj._narrow(CosNaming.NamingContext)
+    # sender
+    def send_thread(chatroom, id):
+        while(1):
+            msg = raw_input("> ")
+            chatroom.send(id, msg)
 
-poa = orb.resolve_initial_references("RootPOA")
+    chatroom = ChatroomClient(sys.argv[2:])
+    listener = ChatroomListener()
+    id = chatroom.register(listener, sys.argv[1])
 
-if rootContext is None:
-    print "Failed to narrow the root naming context"
-    sys.exit(1)
+    Thread(target=send_thread, args=(chatroom, id,)).start()
 
-# Resolve the name "test.my_context/ExampleEcho.Object"
-name = [CosNaming.NameComponent("Chatroom", "")]
-
-try:
-    obj = rootContext.resolve(name)
-
-except CosNaming.NamingContext.NotFound, ex:
-    print "Name not found"
-    sys.exit(1)
-
-# Narrow the object to an Example::Echo
-chatroom = obj._narrow(chat.Chatroom)
-if chatroom is None:
-    print "Object reference is not an Example::Echo"
-    sys.exit(1)
-
-# create and aquire an reference of listener
-listener = Listener_I()
-listenerRef = listener._this()
-
-# register the listener
-id = chatroom.register(listenerRef, sys.argv[1])
-
-Thread(target=send_thread, args=(chatroom, id,)).start()
-
-# run t=flg
-poaManager = poa._get_the_POAManager()
-poaManager.activate()
-
-try:
-    orb.run()
-except KeyboardInterrupt:
-    # unregister the listener and close down the program
-    chatroom.unregister(id)
-    orb.shutdown(True)
     try:
-        sys.exit(0)
-    except SystemExit:
-        os._exit(0)
+        chatroom.run()
+    except KeyboardInterrupt:
+        # unregister the listener and close down the program
+        chatroom.shutdown()
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
